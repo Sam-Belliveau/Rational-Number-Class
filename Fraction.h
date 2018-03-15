db40f6ddf04174ce10dc8c5bc91a82ebbf9cb101
Sampajama <sam.belliveau@gmail.com>
2018-03-15 18:47:59 -0400

    RENAME

---
+++ Fraction.h
@@ -0,0 +1,59 @@
+typedef long Whole; /// Change this to what ever you want
+
+/// This is how much detail is considered when reading floats
+const Whole FLOAT_READING_PRECISION = 10000;
+
+#ifndef FRACTION_H
+#define FRACTION_H
+
+class _Fract
+{
+    public:
+
+        /***** THESE ARE THE ONLY VARS IN THE CLASS *****/
+        Whole N = 0;
+        Whole D = 1;
+
+        /** Constructors **/
+        _Fract();
+        _Fract(Whole topNum, Whole bottomNum);
+        _Fract(const double num);
+
+        /** Casts **/
+        operator Whole();
+        operator double();
+        // I know your not supposed to define in a header
+        _Fract& operator=(const double num){ setFloat(num); }
+
+        /** Operators **/
+
+        // Normal
+        _Fract operator+(const _Fract &num);
+        _Fract operator-(const _Fract &num);
+        _Fract operator*(const _Fract &num);
+        _Fract operator/(const _Fract &num);
+        _Fract operator%(const _Fract &num);
+
+        // Self Modifying
+        void operator+=(const _Fract &num);
+        void operator-=(const _Fract &num);
+        void operator*=(const _Fract &num);
+        void operator/=(const _Fract &num);
+        void operator%=(const _Fract &num);
+
+        // Comparison
+        bool operator==(const _Fract &b);
+        bool operator> (const _Fract &b);
+        bool operator< (const _Fract &b);
+        bool operator>=(const _Fract &b);
+        bool operator<=(const _Fract &b);
+
+    private:
+
+        /** Background work **/
+        Whole GCD(const Whole a, const Whole b);
+        void setFloat(const double num);
+        void simplify();
+};
+
+#endif // FRACTION_H
