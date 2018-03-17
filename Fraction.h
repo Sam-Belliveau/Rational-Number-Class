91a3e3f9ab1c3dfe189b9dcd9663b029f1d7d9fe
Sampajama <sam.belliveau@gmail.com>
2018-03-16 21:44:41 -0400

    Huge Update
    Notes will be in the release

--- Fraction.h
+++ Fraction.h
@@ -1,48 +1,155 @@
-typedef long long Whole; /// Change this to what ever you want
-const Whole FLOAT_READING_PRECISION = 10000;
+typedef long double FPC; /// floating point calculator
 
 #ifndef FRACTION_H
 #define FRACTION_H
 
-class _Fract
+template<typename TYPE>
+class Fract
 {
     public:
         /***** THESE ARE THE ONLY VARS IN THE CLASS *****/
-        Whole N = 0;
-        Whole D = 1;
+        TYPE N = 0;
+        TYPE D = 1;
 
         /** Constructors **/
-        _Fract();
-        _Fract(Whole topNum, Whole bottomNum);
-        _Fract(const double num);
+        Fract(TYPE topNum, TYPE bottomNum) : N{topNum}, D{bottomNum} {}
+
+        Fract(const long double num) { setFloat(num); }
+        Fract(const double num)      { setFloat(num); }
+        Fract(const float num)       { setFloat(num); }
 
         /** Casts **/
-        operator Whole();
-        operator double();
-        void operator=(const double num){ setFloat(num); }
+        operator char()             const { return N/D; }
+        operator short()            const { return N/D; }
+        operator int()              const { return N/D; }
+        operator long int()         const { return N/D; }
+        operator long long int()    const { return N/D; }
+
+        operator long double()  const { return ((long double)N)/((long double)D); }
+        operator double()       const { return ((double)N)/((double)D); }
+        operator float()        const { return ((float)N)/((float)D); }
+
+        void operator=(const long double num)   { setFloat(num); }
+        void operator=(const double num)        { setFloat(num); }
+        void operator=(const float num)         { setFloat(num); }
+
+        void operator=(const char num)      { N = (TYPE)num; D = 1; }
+        void operator=(const short num)     { N = (TYPE)num; D = 1; }
+        void operator=(const int num)       { N = (TYPE)num; D = 1; }
+        void operator=(const long int num)  { N = (TYPE)num; D = 1; }
+        void operator=(const long long num) { N = (TYPE)num; D = 1; }
 
         /** Operators **/
-        _Fract operator+(const _Fract &num);
-        _Fract operator-(const _Fract &num);
-        _Fract operator*(const _Fract &num);
-        _Fract operator/(const _Fract &num);
-        _Fract operator%(const _Fract &num);
-        void operator+= (const _Fract &num);
-        void operator-= (const _Fract &num);
-        void operator*= (const _Fract &num);
-        void operator/= (const _Fract &num);
-        void operator%= (const _Fract &num);
-        bool operator== (const _Fract &b);
-        bool operator>  (const _Fract &b);
-        bool operator<  (const _Fract &b);
-        bool operator>= (const _Fract &b);
-        bool operator<= (const _Fract &b);
+        Fract& operator++()
+        { N += D; return *this; }
+
+        Fract operator++(int)
+        { Fract<TYPE> temp(*this); operator++(); return temp; }
+
+        Fract& operator--()
+        { N -= D; return *this; }
+
+        Fract operator--(int)
+        { Fract<TYPE> temp(*this); operator--(); return temp; }
+
+
+        Fract& operator+=(const Fract<TYPE> &b)
+        {
+            N = N*b.D + D*b.N;
+            D *= b.D;
+            simplify();
+            return *this;
+        }
+
+        friend Fract operator+(Fract<TYPE> a, const Fract<TYPE> &b)
+        { a += b; return a; }
+
+        Fract& operator-=(const Fract<TYPE> &b)
+        {
+            N = N*b.D - D*b.N;
+            D *= b.D;
+            simplify();
+            return *this;
+        }
+
+        friend Fract operator-(Fract<TYPE> a, const Fract<TYPE> &b)
+        { a -= b; return a; }
+
+        Fract& operator%=(const Fract<TYPE> &b)
+        {
+            N = N*b.D % D*b.N;
+            D *= b.D;
+            simplify();
+            return *this;
+        }
+
+        friend Fract operator%(Fract<TYPE> a, const Fract<TYPE> &b)
+        { a %= b; return a; }
+
+        Fract& operator*=(const Fract<TYPE> &b)
+        {
+            N *= b.N;
+            D *= b.D;
+            simplify();
+            return *this;
+        }
+
+        friend Fract operator*(Fract<TYPE> a, const Fract<TYPE> &b)
+        { a *= b; return a; }
+
+        Fract& operator/=(const Fract<TYPE> &b)
+        {
+            N *= b.D;
+            D *= b.N;
+            simplify();
+            return *this;
+        }
+
+        friend Fract operator/(Fract<TYPE> a, const Fract<TYPE> &b)
+        { a /= b; return a; }
+
+
+        friend bool operator< (const Fract<TYPE> &a, const Fract<TYPE> &b)
+        { return (a.N*b.D < a.D*b.N); }
+        /**/
+        /**/friend bool operator>=(const Fract<TYPE> &a, const Fract<TYPE> &b)
+        /**/{ return !(a < b); }
+        /**/
+        /**/friend bool operator> (const Fract<TYPE> &a, const Fract<TYPE> &b)
+        /**/{ return (b < a); }
+        /******/
+        /******/friend bool operator<=(const Fract<TYPE> &a, const Fract<TYPE> &b)
+        /******/{ return !(a > b); }
+
+        friend bool operator==(const Fract<TYPE> &a, const Fract<TYPE> &b)
+        { return (a.N*b.D == a.D*b.N); }
+        /**/
+        /**/friend bool operator!=(const Fract<TYPE> &a, const Fract<TYPE> &b)
+        /**/{ return !(a == b); }
 
     private:
         /** Background work **/
-        Whole GCD(const Whole a, const Whole b);
-        void setFloat(const double num);
-        void simplify();
+        inline TYPE GCD(const TYPE a, const TYPE b)
+        { return b ? GCD(b, a%b) : a; }
+
+        void simplify()
+        {
+            if(D < 0){ D = -D; N = -N; } /// Numerator is what holds the sign
+            if(D == 0){ D = 1; }
+
+            TYPE factor = GCD((N < 0) ? -N : N, D);
+            N /= factor; D /= factor;
+        }
+
+        /// Small enough to avoid overflows,
+        /// enough factors to get good results
+        const TYPE TheHolyGrail = 0x13B0; // does not need to be in hex, I just look like a hacker
+        void setFloat(FPC num)
+        {
+            N = (TYPE)(num * TheHolyGrail + 0.5); /// 0.5 makes the cast round
+            D = TheHolyGrail;
+            simplify();
+        }
 };
 
-#endif // FRACTION_H
+#endif
